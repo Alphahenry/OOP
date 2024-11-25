6aa2cfde4e7f5a9c2568718adbb1fcaaac6a39c6
Alphahenry <alphahenry2019@gmail.com>
2024-11-25 23:08:36 +0300

    added Studentlist class

---
+++ Intro/src/student/StudentList.java
@@ -0,0 +1,12 @@
+package student;
+import  java.util.ArrayList;
+public class StudentList {
+   // initisalize list
+    private ArrayList<Student> students_list;
+
+
+    //constructor
+    public  StudentList(ArrayList<Student> students_list){
+        this.students_list = students_list;
+    }
+}
